725b7999bc831d489339bc89f350faf368ed37e6
Asad Ali <67062577+theasadali@users.noreply.github.com>
2025-03-03 22:49:56 +0500

    Update Sales-Data-Analysis.sql

--- Sales-Data-Analysis.sql
+++ Sales-Data-Analysis.sql
@@ -1,3 +1,26 @@
-use SalesDataAnalysis;
+USE SalesDataAnalysis;
 
-select * from sales_data;
+--fetching everything from sales_data table to analyze columns
+SELECT * FROM sales_data;
+
+--seeing the total sales
+SELECT COUNT(*) FROM sales_data;
+
+--calculating profit
+SELECT ROUND(Unit_Cost,2) AS 'Unit Cost', 
+ROUND(Unit_Price,2) as 'Selling Price', 
+ROUND((Unit_Price - Unit_Cost),2) AS Profit
+FROM sales_data;
+
+--calculating total cost and total profit from sales grouped by categories
+--we can see that Furniture category is the most profited category and in constrast to food which is least profitable category
+--we can also group by region to see the sale and profit of different categories in different regions
+SELECT Product_Category, SUM(ROUND(Unit_Cost,2)) AS 'Total Unit Cost', 
+SUM(ROUND(Unit_Price,2)) as 'Total Selling Price', 
+SUM(ROUND((Unit_Price - Unit_Cost),2)) AS 'Total Profit'
+FROM sales_data
+GROUP BY Product_Category
+ORDER BY 'Total Profit' DESC;
+
+--analysing what payment method most customers prefer
+SELECT Payment_Method, COUNT(*) FROM sales_data GROUP BY Payment_Method;
